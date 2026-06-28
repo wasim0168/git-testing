b39de30112949dda046e246e0bb93463dfa997cd
resicode003 <resicode003@gmail.com>
2026-06-28 16:52:59 +0530

    Fix footer code

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -208,7 +208,7 @@ const FloatingWhatsApp = () => {
 };
 
 // Icon Message component for the quick actions
-const IconMessage = ({ size, className }) => (
+const IconMessage = ({ size, className }: { size: number; className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
